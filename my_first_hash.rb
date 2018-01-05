4b3296534cab3aec666ea8e7efb31a6c429c2fe3
York Wang <yw544@nyu.edu>
2018-01-05 18:16:06 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,7 +22,7 @@ end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
+shipping_manifest["oil paintings"]
   #your code here!
 end
 
